19bfbae9e48ae0ed876d1e389418e39c9489f137
juandpt03 <juandpt03@gmail.com>
2023-05-05 23:08:14 -0500

    SnackBars y dialogs terminados

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      title: 'Flutter Widgets',
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 0).getTheme(),
       routerConfig: appRouter,
